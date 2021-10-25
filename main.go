bdd04b421df2927e49208dba2fdc8f61ad9c6d21
Liam Ross <liamross.code@gmail.com>
2021-10-25 14:27:37 -0700

    fix: cleaning up

--- main.go
+++ main.go
@@ -2,47 +2,28 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"log"
 
-	"github.com/liamross/prisma-issue-repro/prisma/db"
+	"github.com/liamross/prisma-issue-repro/db"
 )
 
 func main() {
-	database := db.NewClient()
-	if err := database.Prisma.Connect(); err != nil {
-		log.Fatalf("error connecting to database")
-	}
+	var (
+		database = db.NewClient()
+		ids      = []string{}
+	)
 
-	defer func() {
-		if err := database.Prisma.Disconnect(); err != nil {
-			log.Fatalf("error disconnecting from database")
-		}
-	}()
+	database.TestValueTwo.FindMany(
 
-	tv1, err := database.TestValueTwo.CreateOne(
-		db.TestValueTwo.TestValueOne.Link(db.TestValueOne.ID.Equals("")),
-	).Exec(context.Background())
-	if err != nil {
-		log.Fatalf("unable to create CustomerSession 1")
-	}
-
-	tv2, err := database.TestValueTwo.CreateOne(
-		db.TestValueTwo.TestValueOne.Link(db.TestValueOne.ID.Equals("")),
-	).Exec(context.Background())
-	if err != nil {
-		log.Fatalf("unable to create CustomerSession 2")
-	}
-
-	ids := []string{tv1.ID, tv2.ID}
-	sessions, err := database.TestValueTwo.FindMany(
-
-		// This is the line that breaks: `In` does not exist.
+		// This is the line that breaks: `In` does not exist, due to the
+		// other array value in the relation `TestValueOne` (the other
+		// array item is `anyOtherArray`). If you remove `anyOtherArray`
+		// from `prisma/schema.prisma` and re-run the generate command,
+		// this will be fixed.
 		db.TestValueTwo.ID.In(ids),
+		//                 ^^ db.TestValueTwo.ID.In undefined (type
+		//                    db.testValueTwoQueryIDString has no field
+		//                    or method In)
+		//                    ------------------------------------------
+		//                    compiler - MissingFieldOrMethod
 	).Exec(context.Background())
-	if err != nil {
-		log.Fatalf("unable to find many CustomerSession")
-	}
-
-	fmt.Println(sessions)
 }
